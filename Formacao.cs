b1140391cfa084ee94e36de36124b964e986b58a
SampaioAllan <lanbrendo93@gmail.com>
2022-04-21 22:02:46 -0300

    Criando propriedades da classe 'Formacao'. Também as encapsulei para que sejam modificadas apenas dentro da  classe, por meio de metodos.

--- Formacao.cs
+++ Formacao.cs
@@ -2,6 +2,48 @@ namespace Universidade
 {
     public class Formacao
     {
+        private string Descricao { get; set; }
+        private int Periodo { get; set; }
+        private double Mensalidade { get; set; }
+        private int Duracao { get; set; }
+
+        public void SetDescricao(string descricao)
+        {
+            Descricao=descricao;
+        }
+
+        public string GetDescricao()
+        {
+            return Descricao;
+        }
+
+        public void SetPeriodo(int periodo)
+        {
+            Periodo=periodo;
+        }
+
+        public int Getperiodo()
+        {
+            return Periodo;
+        }
+
+        public void SetMensalidade(double mensalidade)
+        {
+            Mensalidade=mensalidade;
+        }
+
+        public double GetMensalidade()
+        {
+            return Mensalidade;
+        }
         
+        public void SetDuracao(int duracao)
+        {
+            Duracao=duracao;
+        }
+        public int GetDuracao()
+        {
+            return Duracao;
+        }        
     }
 }
